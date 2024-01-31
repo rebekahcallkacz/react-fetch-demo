c06e3fb6f6a210165e705b01a02771b5c3cc2400
Rebekah Callari-Kaczmarczyk <rebekahcallari@gmail.com>
2024-01-30 19:42:25 -0500

    add test for additional edge cases

--- src/utils.test.js
+++ src/utils.test.js
@@ -16,12 +16,38 @@ const PERSON_2 = {
 
 const EXPECTED_PERSON_LABEL_2 = "Rebekah | female | unknown";
 
+const PERSON_3 = {
+  name: undefined,
+  gender: "female",
+  mass: 70,
+};
+
+const EXPECTED_PERSON_LABEL_3 = "unknown | female | 70";
+
+const PERSON_4 = {
+  name: "Rebekah",
+  gender: null,
+  mass: undefined,
+};
+
+const EXPECTED_PERSON_LABEL_4 = "Rebekah | unknown | unknown";
+
 test("returns formatted person string", () => {
   const personLabel = createButtonLabel(PERSON_1);
   expect(personLabel).toEqual(EXPECTED_PERSON_LABEL_1);
 });
 
-test("returns formatted person string when mass is unknown", () => {
+test("returns formatted person string when mass is undefined", () => {
   const personLabel = createButtonLabel(PERSON_2);
   expect(personLabel).toEqual(EXPECTED_PERSON_LABEL_2);
+});
+
+test("returns formatted person string when name is undefined", () => {
+  const personLabel = createButtonLabel(PERSON_3);
+  expect(personLabel).toEqual(EXPECTED_PERSON_LABEL_3);
+});
+
+test("returns formatted person string when gender is null", () => {
+  const personLabel = createButtonLabel(PERSON_4);
+  expect(personLabel).toEqual(EXPECTED_PERSON_LABEL_4);
 });
